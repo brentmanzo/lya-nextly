dceed3304cd7cb9172599da318ccf601c57cd9c6
Brent <millen.brent@gmail.com>
2026-09-07 03:08:08 +0800

    default to light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${plusJakarta.variable} font-sans`}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+        >
           <Navbar />
           <div>{children}</div>
           <Footer />
