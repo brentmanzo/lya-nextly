de894bae7de1dcfba9cc46c0fd5b29e3ec6911d0
Brent <millen.brent@gmail.com>
2026-09-10 06:36:05 +0800

    update company profile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import heroImg from "../../public/img/hero-1.jpg";
 export const Hero = () => {
   return (
     <>
-      <Container className="flex flex-wrap ">
+      <Container className="flex flex-wrap lg:pt-0">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
@@ -15,9 +15,9 @@ export const Hero = () => {
               HVAC & Air Filtration Solutions, Built for Better Air
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            <div className="flex flex-col items-center lg:items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                href="/company-profile.pdf"
                 target="_blank"
                 rel="noopener"
                 className="btn-primary px-8 py-4 text-center text-lg font-medium"
